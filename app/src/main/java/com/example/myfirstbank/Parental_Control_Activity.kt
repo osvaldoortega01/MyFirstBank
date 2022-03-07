4e22a8030fdfb5b0785d4df4df5f6b1f8a95ad07
Jeac1702 <jeac1702@gmail.com>
2022-03-06 18:45:21 -0600

    Create parental control

---
+++ app/src/main/java/com/example/myfirstbank/Parental_Control_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Parental_Control_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parental_control)
+    }
+}
